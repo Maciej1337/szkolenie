d6828547d90ef9433a8923c5d6e520674c41f406
Maciej1337 <Maciej@192.168.1.108>
2015-01-26 16:20:41 +0100

    Pierwsza zmiana

--- ListShop/src/com/example/listshop/MainActivity.java
+++ ListShop/src/com/example/listshop/MainActivity.java
@@ -19,6 +19,8 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
  
         list = (ListView) findViewById(R.id.listView1);
+        
+        //Dodaæ tutaj jakieœ pozycje
  
         String cars[] = {"Mercedes", "Fiat", "Ferrari", "Aston Martin"};
  
